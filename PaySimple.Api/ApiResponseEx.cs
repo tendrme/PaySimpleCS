25bc1f84b42c97599b15fd3a5bed0ab85a1ce2c3
abe <abe@dpulp.com>
2013-05-22 15:03:49 -0400

    Added exception helpers.

---
+++ PaySimple.Api/ApiResponseEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+using PaySimple.Api.Types;
+
+namespace PaySimple.Api
+{
+    public static class ApiResponseEx
+    {
+        public static void ThrowIfError<T>(this ApiResponse<T> response)
+            where T : class
+        {
+            if (response.Meta.Errors != null)
+                throw new PaySimpleException<T>(response);
+        }
+    }
+}
---
+++ PaySimple.Api/PaySimpleException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+using PaySimple.Api.Types;
+
+namespace PaySimple.Api
+{
+    public class PaySimpleException<T> : Exception where T : class
+    {
+        readonly ApiResponse<T> _response;
+
+        public PaySimpleException(ApiResponse<T> response)
+        {
+            _response = response;
+        }
+
+        public ErrorMessage[] Messages
+        {
+            get { return _response.Meta.Errors.ErrorMessages; }
+        }
+    }
+}
